01d7395925b5ee7d1e69d2758b6ca59131e4478b
stars_inc <stars_inc@mail.ru>
2021-02-28 01:17:28 +0300

    feat: solve brackets-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
   const config = bracketsConfig.map(el => el.join``)
 
-  for(let i = 0; config.length; i++) {
+  for (let i = 0; i < config.length; i++) {
     if(str.includes(config[i])) {
       str = str.replace(config[i], '')
       i = -1
     }
   }
-
   return !str
 }
